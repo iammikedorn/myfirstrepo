ca6b79e43faa7a448de0e7f9f65a941e725fd4a1
Mike Dorn <mikedorn@gmail.com>
2017-11-03 20:25:19 -0500

    test

--- hello.py
+++ hello.py
@@ -1,12 +1,5 @@
 # This program says hello and asks for my name.
-import sys; print('Python %s on %s' % (sys.version, sys.platform))
+
 print('Hello world!')
-print('What is your name?')    # ask for their name
-myName = input()
-print('It is good to meet you, ' + myName)
-print('The length of your name is:')
-print(len(myName))
-print('What is your age?')    # ask for their age
-myAge = input()
-print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
 
