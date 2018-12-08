15132e3561d5e27fa6d6a24c2ec352251e14a493
mikedorn <mikedorn@gmail.com>
2018-12-08 00:25:51 -0600

    Add files via upload

---
+++ pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['Pepperoni', 'Cheese', 'Sausage']
+
+for pizza in pizzas:
+    print("I love " + pizza.lower()+" pizza")
+
+print("I love the cheese and the sauce when it comes out of the oven,\nI really love pizza")
